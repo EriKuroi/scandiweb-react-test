cbc6b120e501719d71abb9a52393b7e3a94161a2
Eri <stayagolubcov@gmail.com>
2021-04-28 17:05:37 +0300

    Fix: dependencies fix Test favicon Add deploy to gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,7 @@ module.exports = {
       title: 'React-test-scandiweb',
       filename: 'index.html',
       template: './src/index.html',
+      favicon: './src/assets/favicon.ico',
     }),
   ],
 };
